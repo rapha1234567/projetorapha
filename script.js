cd2af64923d66fb13b1de8bca7b84930c4b406cc
raphinha12345 <baranovschi.oliveira@escola.pr.gov.br>
2025-06-18 21:51:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Adicionando rolagem suave para links do menu
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
